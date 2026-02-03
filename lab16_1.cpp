ea5030b25c7ebfcea1955cf4e6ee1db65e2c913a
sila_senapong <sila_sanapong@cmu.ac.th>
2026-02-03 20:24:11 +0700

    lab16

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,34 +1,93 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<iomanip>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 using namespace std;
 
-void showData(double *,int,int);
+void showData(double *, int, int);
 
-void randData(double *,int,int);
+void randData(double *, int, int);
 
-void findRowSum(const double *,double *,int,int);
+void findRowSum(const double *, double *, int, int);
 
-void findColSum(const double *,double *,int,int);
+void findColSum(const double *, double *, int, int);
 
-int main(){
+int main()
+{
 	srand(time(0));
 	const int N = 6, M = 8;
 	double data[N][M] = {};
 	double sum1[N] = {};
 	double sum2[M] = {};
 	double *dPtr = data[0];
-	randData(dPtr,N,M);
-	showData(dPtr,N,M);
-	
+	randData(dPtr, N, M);
+	showData(dPtr, N, M);
+
 	cout << "---------------------------------------------\n";
-	
-	findRowSum(dPtr,sum1,N,M);
-	showData(sum1,N,1);
-	
+
+	findRowSum(dPtr, sum1, N, M);
+	showData(sum1, N, 1);
+
 	cout << "---------------------------------------------\n";
-	
-	findColSum(dPtr,sum2,N,M); 
-	showData(sum2,1,M);
+
+	findColSum(dPtr, sum2, N, M);
+	showData(sum2, 1, M);
+}
+
+void showData(double *dPtr, int N, int M)
+{
+	cout << fixed << setprecision(2);
+	for (int i = 0; i < N * M; i++)
+	{
+		cout << dPtr[i];
+
+		if ((i + 1) % M == 0)
+		{
+			cout << endl;
+		}
+		else
+		{
+			cout << " ";
+		}
+	}
+}
+
+void randData(double *dPtr, int N, int M)
+{
+	for (int i = 0; i < N * M; i++)
+	{
+		dPtr[i] = (rand() % 101) / 100.0;
+	}
+}
+
+void findRowSum(const double *dPtr, double *sum1, int N, int M)
+{
+	double sum = 0.0;
+	int index = 0;
+
+	for (int i = 0; i < N * M; i++)
+	{
+		sum += *(dPtr + i);
+
+		if ((i + 1) % M == 0)
+		{
+			sum1[index] = sum;
+			sum = 0.0;
+			index++;
+		}
+	}
+}
+
+void findColSum(const double *dPtr, double *sum1, int N, int M)
+{
+	for (int j = 0; j < M; j++)
+	{
+		sum1[j] = 0.0;
+	}
+
+	for (int i = 0; i < N * M; i++)
+	{
+		int colIndex = i % M;
+		sum1[colIndex] += *(dPtr + i);
+	}
 }
